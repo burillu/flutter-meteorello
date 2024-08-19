0784694030de2474e9a970523f3d405b3ecdb120
burillu <burillu90@hotmail.it>
2024-08-19 18:41:09 +0200

    first commit

---
+++ android/app/src/main/kotlin/com/example/meteorello/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.meteorello
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ lib/main.dart
@@ -0,0 +1,6 @@
+import 'package:flutter/material.dart';
+import 'package:meteorello/app.dart';
+
+void main() {
+  runApp(App());
+}
